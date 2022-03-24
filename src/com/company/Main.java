32a31d2eadc5dcfd3c80ae937d71a2398af3308a
DanyloKovalchyk <101935665+DanyloKovalchyk@users.noreply.github.com>
2022-03-24 12:49:31 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Завдання 2
+        int n = 123;
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int a1;
+        int b1;
+        int c1;
+
+        a1 = n % 10;
+        a = a+a1;
+
+
+        b1 = n % 100;
+        b = b+b1;
+        b1 = b1/10;
+
+
+        c1 = n%1000;
+        c = c+c1;
+        c1 = c1/100;
+
+
+        if((a1 >b1) && (a1>c1))
+            System.out.println(a1 + " Це число найбільше.");
+        if((b1 >a1) && (b1>c1))
+            System.out.println(b1 +" Це число найбільше.");
+        if((c1>a1) && (c1>b1))
+            System.out.println(c1 +" Це число найбільше.");
+    }
+}
